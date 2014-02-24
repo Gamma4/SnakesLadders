79c5892f25be663ec1d656c8acaf9b2fd2ec19a5
Ryan <rbyrne@bounde.co.uk>
2014-02-24 18:18:31 +0000

    Added Gameboard

--- ConsoleApplication1/ConsoleApplication1/snakes.c
+++ ConsoleApplication1/ConsoleApplication1/snakes.c
@@ -6,13 +6,33 @@ int boardSize = 10;
 int Repetitions = 10000;
 int position = -1;
 int newPosition = 0;
-int gameBoard[16] = { 0, 0, 0, 6, 0, 0, 1, 0, -9, 0, 0, 0, 0, 0, 0, 0 };
+int gameBoard[101] = { 0 };
+int snakesLadders[19][2] = {
+	{ 1, 37 },
+	{ 4, 10 },
+	{ 9, 22 },
+	{ 21, 21 },
+	{ 28, 56 },
+	{ 36, 8 },
+	{ 51, 16 },
+	{ 71, 20 },
+	{ 80, 20 },
+	{ 87, 63 }, 
+	{ 16, -10 },
+	{ 47, -26 },
+	{ 49, -38 },
+	{ 56, -3 },
+	{ 62, -43 },
+	{ 64, -4 },
+	{ 93, -20 },
+	{ 95, -20 },
+	{ 98, -20 },
+};
 int gameSquareCounter[10] = { 0 };
 int dieRollHistory[10] = { 0 };
 int minRolls = 1000;
 int i = 0;
 int k = 0;
-
 int dieRolls[5] = { 0 };
 
 int rollDie(){
@@ -23,15 +43,16 @@ int rollDie(){
 	return diceroll;
 };
 
+int initGameBoard(){
+	for (i = 0; i < 19; i++){
+		gameBoard[snakesLadders[i][0]] = snakesLadders[i][1];
+	}
+}
+
 int main(){	
 
-	FILE * fp;
+	initGameBoard();
 
-	fp = fopen("hash.txt", "w+");
-	fprintf(fp, "Hello, I'm a PC!");
-
-	fclose(fp);
-	
 	time_t t;
 	srand((unsigned)time(&t));
 
@@ -40,11 +61,10 @@ int main(){
 		int dieRolls = 0;
 		int path[1000] = { 0 };
 
-
 		while (position <= 10 && dieRolls < 10000){
 			//While position = n-6
 			while (position <= 5 && dieRolls < 10000){
-				position = position + rollDie();
+				position = position + rollDie(1000);
 				position = position + gameBoard[position];
 				gameSquareCounter[position]++;
 				dieRolls++;
